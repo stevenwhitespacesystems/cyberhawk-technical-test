a6e30f3a9fb6abe0b38e208427718603a651dde2
Steven McGill <steven@whitespacesystems.co.uk>
2024-11-13 14:03:46 +0000

    Added prettier and eslint with airbnb stylings

--- resources/js/App.tsx
+++ resources/js/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const App: React.FC = () => {
+function App() {
     return (
         <div>
             <h1>Hello from React + TypeScript!</h1>
         </div>
     );
-};
+}
 
 export default App;
