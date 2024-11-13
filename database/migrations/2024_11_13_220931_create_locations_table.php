031e249920ab0c0f764d2091a557bbae70912d06
Steven McGill <steven@whitespacesystems.co.uk>
2024-11-13 22:16:45 +0000

    initial location migration

--- database/migrations/2024_11_13_220931_create_locations_table.php
+++ database/migrations/2024_11_13_220931_create_locations_table.php
@@ -14,8 +14,21 @@ return new class extends Migration
     public function up()
     {
         Schema::create('locations', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->ulid('id')->primary();
+            $table->string('name');
+            $table->string('short_identifier');
+            $table->string('description')->nullable();
+            $table->string('address_line_1');
+            $table->string('address_line_2')->nullable();
+            $table->string('city');
+            $table->string('state');
+            $table->string('postal_code');
+            $table->string('country');
+            $table->string('country_code', 2);
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 10, 7);
+            $table->softDeletesTz();
+            $table->timestampsTz();
         });
     }
 
