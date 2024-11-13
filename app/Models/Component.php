42a0cb7f607ac8abab75b22cc293ebaca3d79b68
Steven McGill <steven@whitespacesystems.co.uk>
2024-11-13 23:56:57 +0000

    initial commit of component resources

---
+++ app/Models/Component.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Component extends Model
+{
+    use HasFactory;
+    use HasUlids;
+}
