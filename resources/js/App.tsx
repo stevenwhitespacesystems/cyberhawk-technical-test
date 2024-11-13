f43b29e65858f93e4ac94fdc07a902fc0deb19d4
Steven McGill <steven@whitespacesystems.co.uk>
2024-11-13 14:48:36 +0000

    added shadcn/ui

--- resources/js/App.tsx
+++ resources/js/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
     return (
         <div className="min-h-screen bg-gray-100 flex items-center justify-center">
             <h1 className="text-3xl font-bold text-gray-800">
                 Hello from React + TypeScript + Tailwind CSS!
             </h1>
+            <Button>Click me</Button>
         </div>
     );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
     plugins: [
@@ -19,7 +20,7 @@ export default defineConfig({
     resolve: {
         extensions: [".js", ".jsx", ".ts", ".tsx"],
         alias: {
-            "@": "/resources/js",
+            "@": path.resolve(__dirname, "./resources/js"),
         },
     },
 });
