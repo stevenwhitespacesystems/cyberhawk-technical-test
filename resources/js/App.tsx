1b733f91ab2110d1327147f8023f6a5202c0d388
Steven McGill <steven@whitespacesystems.co.uk>
2024-11-13 14:33:50 +0000

    added tailwindcss

--- resources/js/App.tsx
+++ resources/js/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-
 function App() {
     return (
-        <div>
-            <h1>Hello from React + TypeScript!</h1>
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+            <h1 className="text-3xl font-bold text-gray-800">
+                Hello from React + TypeScript + Tailwind CSS!
+            </h1>
         </div>
     );
 }
