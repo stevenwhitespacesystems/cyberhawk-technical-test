b71d1c9c86d2bfda99bf8d3633ed8cad5b0c215f
Steven McGill <steven@whitespacesystems.co.uk>
2024-11-13 14:09:55 +0000

    added ecs with psr12 checking

---
+++ ecs.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return ECSConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap',
+        __DIR__ . '/config',
+        __DIR__ . '/lang',
+        __DIR__ . '/public',
+        __DIR__ . '/resources',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ])
+
+    // add PSR-12 rules
+    ->withPreparedSets(
+        psr12: true
+    )
+
+    // add a single rule
+    ->withRules([
+        NoUnusedImportsFixer::class,
+    ]);
