45d354d516fd821de39738efc5756f1912e50b20
Steven McGill <steven@whitespacesystems.co.uk>
2024-11-14 22:52:19 +0000

    Helper class to handle different API Responses

---
+++ app/Http/Responses/ApiResponses.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Responses;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+final class ApiResponses
+{
+    public static function validationError(Validator $validator): JsonResponse
+    {
+        return new JsonResponse([
+            'message' => 'Validation errors',
+            'data' => $validator->errors(),
+        ], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /**
+     * @param array<string, mixed> $data
+     */
+    public static function success(array $data = [], int $status = Response::HTTP_OK): JsonResponse
+    {
+        return new JsonResponse([
+            'success' => true,
+            'data' => $data,
+        ], $status);
+    }
+}
