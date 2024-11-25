eab26342e09e59f64bdb2ac6cfcdeb87d4814583
Steven McGill <steven@whitespacesystems.co.uk>
2024-11-25 17:08:44 +0000

    dead code

---
+++ resources/js/tables/components/columns.tsx
@@ -0,0 +1,95 @@
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+
+export type Component = {
+    id: string;
+    name: string;
+    short_identifier: string;
+};
+
+export const columns: ColumnDef<Component>[] = [
+    {
+        id: "site.name",
+        accessorKey: "site.name",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Site
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        meta: {
+            filterVariant: "text",
+        },
+    },
+    {
+        id: "equipment.nickname",
+        accessorKey: "equipment.nickname",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Equipment
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        meta: {
+            filterVariant: "text",
+        },
+    },
+    {
+        id: "equipment.serial_number",
+        accessorKey: "equipment.serial_number",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Equipment Serial Number
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        meta: {
+            filterVariant: "text",
+        },
+    },
+    {
+        id: "equipment.serial_number",
+        accessorKey: "equipment.serial_number",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Equipment Serial Number
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        meta: {
+            filterVariant: "text",
+        },
+    },
+    // {
+    //     id: "type",
+    //     accessorKey: "type",
+    //     header: ({ column }) => (
+    //         <Button
+    //             variant="ghost"
+    //             onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+    //         >
+    //             Type
+    //             <ArrowUpDown className="ml-2 h-4 w-4" />
+    //         </Button>
+    //     ),
+    //     cell: ({ cell }) => {
+    //         // const value = cell.getValue() as EquipmentType;
+    //         // return EquipmentTypeUtils.label(value);
+    //         return cell.getValue();
+    //     },
+    //     enableColumnFilter: false,
+    // },
+];
