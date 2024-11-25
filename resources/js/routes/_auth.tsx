ea168ebb785875d5f1a359603f15d42c9a662863
Steven McGill <steven@whitespacesystems.co.uk>
2024-11-25 22:48:26 +0000

    Bugfix: wasn't handling unauthenticated correctly

--- resources/js/routes/_auth.tsx
+++ resources/js/routes/_auth.tsx
@@ -10,16 +10,25 @@ export const Route = createFileRoute("/_auth")({
     beforeLoad: async ({ location }) => {
         const token = useAuthStore.getState().token;
         if (token) {
-            const response = await axios.get("/api/auth/verify", {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
+            try {
+                const response = await axios.get("/api/auth/verify", {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
 
-            if (response.status === 200) {
-                useAuthStore.setState({ token, user: response.data.data.user });
-                axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-                return;
+                if (response.status === 200) {
+                    useAuthStore.setState({ token, user: response.data.data.user });
+                    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+                    return;
+                }
+            } catch (error) {
+                throw redirect({
+                    to: "/login",
+                    search: {
+                        redirect: location.href,
+                    },
+                });
             }
         }
 
