aa257472e4b8d364c31e0b7bd4c67c3c0128c0a0
Steven McGill <steven@whitespacesystems.co.uk>
2024-11-26 00:31:13 +0000

    Scaffolding dialogs

--- resources/js/routes/_auth.inspections.tsx
+++ resources/js/routes/_auth.inspections.tsx
@@ -6,6 +6,19 @@ import { useTableData } from "@/hooks/use-table-data";
 import { fetchDataForTable } from "@/api/table";
 import { columns } from "@/tables/inspections/columns";
 import { DataTable } from "@/tables/root/data-table";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 export const Route = createFileRoute("/_auth/inspections")({
     component: RouteComponent,
@@ -52,6 +65,50 @@ function RouteComponent() {
                     }}
                     setPagination={setPagination}
                     rowCount={tableData?.meta.total}
+                    globalActions={
+                        <Dialog>
+                            <DialogTrigger asChild>
+                                <Button>
+                                    <Plus />
+                                    Create
+                                </Button>
+                            </DialogTrigger>
+                            <DialogContent className="sm:max-w-[425px]">
+                                <DialogHeader>
+                                    <DialogTitle>Edit profile</DialogTitle>
+                                    <DialogDescription>
+                                        Make changes to your profile here. Click save when you're
+                                        done.
+                                    </DialogDescription>
+                                </DialogHeader>
+                                <div className="grid gap-4 py-4">
+                                    <div className="grid grid-cols-4 items-center gap-4">
+                                        <Label htmlFor="name" className="text-right">
+                                            Name
+                                        </Label>
+                                        <Input
+                                            id="name"
+                                            defaultValue="Pedro Duarte"
+                                            className="col-span-3"
+                                        />
+                                    </div>
+                                    <div className="grid grid-cols-4 items-center gap-4">
+                                        <Label htmlFor="username" className="text-right">
+                                            Username
+                                        </Label>
+                                        <Input
+                                            id="username"
+                                            defaultValue="@peduarte"
+                                            className="col-span-3"
+                                        />
+                                    </div>
+                                </div>
+                                <DialogFooter>
+                                    <Button type="submit">Save changes</Button>
+                                </DialogFooter>
+                            </DialogContent>
+                        </Dialog>
+                    }
                 />
             </div>
         </>
--- resources/js/tables/inspections/columns.tsx
+++ resources/js/tables/inspections/columns.tsx
@@ -1,8 +1,19 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { InspectionStatus, InspectionStatusUtils } from "@/enums/inspection-statuses";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowRight, ArrowUpDown, Trash } from "lucide-react";
+import { isNaN } from "lodash";
+import { ArrowRight, ArrowUpDown } from "lucide-react";
 
 export type Inspection = {
     id: string;
@@ -25,6 +36,17 @@ export const columns: ColumnDef<Inspection>[] = [
         ),
         meta: {
             filterVariant: "date",
+        },
+        cell({ cell }) {
+            const value = cell.getValue();
+            if (typeof value === "string" || typeof value === "number") {
+                const date = new Date(value);
+                if (isNaN(date.getTime())) {
+                    return ""; // Invalid date
+                }
+                return date.toLocaleDateString();
+            }
+            return ""; // Unknown type
         },
     },
     {
@@ -70,25 +92,6 @@ export const columns: ColumnDef<Inspection>[] = [
         ),
     },
     {
-        id: "status",
-        accessorKey: "status",
-        header: ({ column }) => (
-            <Button
-                variant="ghost"
-                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-            >
-                Status
-                <ArrowUpDown className="ml-2 h-4 w-4" />
-            </Button>
-        ),
-        cell: ({ cell }) => {
-            const value = cell.getValue() as InspectionStatus;
-            const badgeConfig = InspectionStatusUtils.badgeConfig(value);
-            return <Badge variant={badgeConfig.variant}>{badgeConfig.label}</Badge>;
-        },
-        enableColumnFilter: false,
-    },
-    {
         id: "completed_date",
         accessorKey: "completed_date",
         header: ({ column }) => (
@@ -103,19 +106,91 @@ export const columns: ColumnDef<Inspection>[] = [
         meta: {
             filterVariant: "date",
         },
+        cell({ cell }) {
+            const value = cell.getValue();
+            if (typeof value === "string" || typeof value === "number") {
+                const date = new Date(value);
+                if (isNaN(date.getTime())) {
+                    return ""; // Invalid date
+                }
+                return date.toLocaleDateString();
+            }
+            return ""; // Unknown type
+        },
+    },
+    {
+        id: "grade",
+        accessorKey: "grade",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Avg Component Grade
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        meta: {
+            filterVariant: "number",
+        },
+        cell: ({ cell }) => {
+            const value = cell.getValue() as number;
+            let jsx = null;
+            if (value >= 3.5) {
+                jsx = <Badge variant="default">{value}</Badge>;
+            } else if (value >= 3.0) {
+                jsx = <Badge variant="secondary">{value}</Badge>;
+            } else {
+                jsx = <Badge variant="destructive">{value}</Badge>;
+            }
+            return <div className="flex justify-center">{jsx}</div>;
+        },
     },
     {
         id: "actions",
         cell: () => (
             <div className="flex justify-end gap-2">
-                <Button variant="outline">
-                    <ArrowRight />
-                    View
-                </Button>
-                <Button variant="destructive">
-                    <Trash />
-                    Delete
-                </Button>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button variant="outline">
+                            <ArrowRight />
+                            View
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                            <DialogTitle>Edit profile</DialogTitle>
+                            <DialogDescription>
+                                Make changes to your profile here. Click save when you are done.
+                            </DialogDescription>
+                        </DialogHeader>
+                        <div className="grid gap-4 py-4">
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="name" className="text-right">
+                                    Name
+                                </Label>
+                                <Input
+                                    id="name"
+                                    defaultValue="Pedro Duarte"
+                                    className="col-span-3"
+                                />
+                            </div>
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor="username" className="text-right">
+                                    Username
+                                </Label>
+                                <Input
+                                    id="username"
+                                    defaultValue="@peduarte"
+                                    className="col-span-3"
+                                />
+                            </div>
+                        </div>
+                        <DialogFooter>
+                            <Button type="submit">Save changes</Button>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
             </div>
         ),
     },
