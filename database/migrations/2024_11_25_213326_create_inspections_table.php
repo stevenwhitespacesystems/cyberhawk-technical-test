b40e699f668476e6a4b759c60a74364553b85cbe
Steven McGill <steven@whitespacesystems.co.uk>
2024-11-26 16:19:44 +0000

    Added Inspection reference field

--- database/migrations/2024_11_25_213326_create_inspections_table.php
+++ database/migrations/2024_11_25_213326_create_inspections_table.php
@@ -5,6 +5,7 @@ use App\Models\Site;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -20,12 +21,16 @@ return new class extends Migration
             $table->ulid('id')->primary();
             $table->foreignIdFor(Site::class)->constrained();
             $table->foreignIdFor(Equipment::class)->constrained();
+            $table->unsignedInteger('reference')->unique();
             $table->date('scheduled_date')->nullable();
             $table->date('completed_date')->nullable();
             $table->unsignedDecimal('grade', 4, 1)->nullable();
             $table->softDeletesTz();
             $table->timestampsTz();
         });
+
+        DB::statement('ALTER TABLE inspections MODIFY reference INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT');
+        DB::statement('ALTER TABLE inspections AUTO_INCREMENT = 10000');
     }
 
     /**
